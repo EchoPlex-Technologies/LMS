c7a0cda3b1b4c97d147baca7cd43bc152c8bd473
Joseph Istvan <josephistvan312@gmail.com>
2025-12-25 07:38:23 +0300

    confiured the automatic dark/light mode colour settings

--- elearning-frontend/src/components/Header/Header.jsx
+++ elearning-frontend/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router';
 import './Header.css'
 import LogoLight from '../../assets/images/logos/logo-light-Photoroom.png'
+import LogoDark from '../../assets/images/logos/logo-dark-Photoroom.png'
 import UserTools from './UserTools';
 
 
@@ -9,7 +10,11 @@ function Header() {
     <header className="header">
       <div className="left-section">
         <Link to='/' className="logo-section">
-          <img className='logo' src={LogoLight} alt="LMS+" />
+          <picture>
+            <source media="(prefers-color-scheme: dark)" srcSet={LogoDark} />
+            <img className='logo' src={LogoLight} alt="LMS+" />
+          </picture>
+
         </Link>
       </div>
       <div className="middle-section"></div>
